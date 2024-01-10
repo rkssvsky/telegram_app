ac3694cd25810f0f53d86ad15c75d62d2aa52899
Суяргулов Радмир Юлаевич <radmir.suyargulov@ocrv.ru>
2024-01-10 15:27:09 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  base: 'test'
 })
+
+
